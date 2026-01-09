a898f68fddaaf661af57f287d02cb8558b5da5d3
kuro1999 <edoardo.marchionni99@gmail.com>
2026-01-09 19:02:49 +0100

    forse file corrotto? se va iBGP ed eBGP fatto

---
+++ scripts/routing/bgp_r101.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+vtysh <<'VEOF'
+conf t
+router bgp 100
+ bgp router-id 1.255.0.1
+ no bgp ebgp-requires-policy
+ neighbor 1.255.0.2 remote-as 100
+ neighbor 1.255.0.2 update-source lo
+ neighbor 1.255.0.3 remote-as 100
+ neighbor 1.255.0.3 update-source lo
+ !
+ address-family ipv4 unicast
+  neighbor 1.255.0.2 activate
+  neighbor 1.255.0.3 activate
+  network 1.0.101.0/30
+ exit-address-family
+end
+write memory
+VEOF
