cc009b241e035aac83702f76ce5f5be7d1c1a666
polidori1999 <leo.polidori99@gmail.com>
2026-01-09 18:28:11 +0100

    aggiunto file bgp

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -381,5 +381,109 @@ write memory
 VEOF
 EOF
 
+
+
+cat > routing/bgp_r101.sh <<'EOF'
+#!/usr/bin/env bash
+set -euo pipefail
+vtysh <<'VEOF'
+conf t
+router bgp 100
+ bgp router-id 1.255.0.1
+ no bgp ebgp-requires-policy
+ neighbor 1.255.0.2 remote-as 100
+ neighbor 1.255.0.2 update-source lo
+ neighbor 1.255.0.3 remote-as 100
+ neighbor 1.255.0.3 update-source lo
+ !
+ address-family ipv4 unicast
+  neighbor 1.255.0.2 activate
+  neighbor 1.255.0.3 activate
+  network 1.0.101.0/30
+ exit-address-family
+end
+write memory
+VEOF
+EOF
+
+cat > routing/bgp_r102.sh <<'EOF'
+#!/usr/bin/env bash
+set -euo pipefail
+vtysh <<'VEOF'
+conf t
+router bgp 100
+ bgp router-id 1.255.0.2
+ no bgp ebgp-requires-policy
+ neighbor 1.255.0.1 remote-as 100
+ neighbor 1.255.0.1 update-source lo
+ neighbor 1.255.0.3 remote-as 100
+ neighbor 1.255.0.3 update-source lo
+ !
+ address-family ipv4 unicast
+  neighbor 1.255.0.1 activate
+  neighbor 1.255.0.3 activate
+  network 1.0.102.0/30
+ exit-address-family
+end
+write memory
+VEOF
+
+EOF
+
+
+cat > routing/bgp_r103.sh <<'EOF'
+#!/usr/bin/env bash
+set -euo pipefail
+vtysh <<'VEOF'
+conf t
+router bgp 100
+ bgp router-id 1.255.0.3
+ no bgp ebgp-requires-policy
+ ! iBGP
+ neighbor 1.255.0.1 remote-as 100
+ neighbor 1.255.0.1 update-source lo
+ neighbor 1.255.0.2 remote-as 100
+ neighbor 1.255.0.2 update-source lo
+ ! eBGP verso AS200
+ neighbor 10.0.31.2 remote-as 200
+ !
+ address-family ipv4 unicast
+  neighbor 1.255.0.1 activate
+  neighbor 1.255.0.2 activate
+  neighbor 10.0.31.2 activate
+  neighbor 1.255.0.1 next-hop-self
+  neighbor 1.255.0.2 next-hop-self
+ exit-address-family
+end
+write memory
+VEOF
+
+EOF
+
+
+cat > routing/bgp_r201.sh <<'EOF'
+#!/usr/bin/env bash
+set -euo pipefail
+vtysh <<'VEOF'
+conf t
+! (opzionale) rotta verso DMZ per poterla annunciare via BGP
+ip route 2.80.200.0/24 10.0.200.2
+!
+router bgp 200
+ bgp router-id 2.255.0.1
+ no bgp ebgp-requires-policy
+ neighbor 10.0.31.1 remote-as 100
+ !
+ address-family ipv4 unicast
+  neighbor 10.0.31.1 activate
+  network 2.255.0.1/32
+  network 2.80.200.0/24
+ exit-address-family
+end
+write memory
+VEOF
+
+EOF
+
 chmod +x setup/*.sh routing/*.sh
 echo "OK: creati setup/ e routing/. Ora esegui gli script dentro i nodi GNS3."
